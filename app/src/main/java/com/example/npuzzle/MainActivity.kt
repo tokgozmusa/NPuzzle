5bc26d652b3c545a83b84538df649210e18df8f2
tokgozmusa <tokgozmusa@gmail.com>
2017-10-28 14:31:22 +0300

    Add fragments

--- app/src/main/java/com/example/npuzzle/MainActivity.kt
+++ app/src/main/java/com/example/npuzzle/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.npuzzle
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val manager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showMainFragment()
+
+        button_start.setOnClickListener {
+            showPuzzleFragment()
+        }
+    }
+
+    fun showMainFragment() {
+        val transaction = manager.beginTransaction();
+        val fragment = MainFragment()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.commit()
+    }
+
+    fun showPuzzleFragment() {
+        val transaction = manager.beginTransaction();
+        val fragment = PuzzleFragment()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+
     }
 }
---
+++ app/src/main/java/com/example/npuzzle/PuzzleFragment.kt
@@ -0,0 +1,67 @@
+package com.example.npuzzle
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by tokgozmusa on 28/10/2017.
+ */
+
+class PuzzleFragment : Fragment() {
+
+    val TAG = "PuzzleFragment"
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater!!.inflate(R.layout.fragment_puzzle, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+
+
+}
